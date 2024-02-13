53e6bf8cec36579efae427354ee968b52ded15f2
Matheus Ferreira <matheus.fer33@gmail.com>
2024-02-13 15:36:42 -0300

    chore: added db connection to mongodb

--- server/src/shared/http/server.ts
+++ server/src/shared/http/server.ts
@@ -2,17 +2,23 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import { connectDB } from "./db";
+
 const app = express();
 
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 7000;
+connectDB();
 
+// routes
 app.get("/api/test", (req: Request, res: Response) => {
   res.json({ message: "Hello from server!" });
 });
+
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
